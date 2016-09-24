2b82093b4d78ca0060f615d38ab8981728ee0793
Joshua <jacobj10@bbhcsd.org>
2016-09-24 17:17:49 -0400

    added breakdown!

---
+++ routes/breakdown.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+
+router.get('/', function(req, res) {
+        var url = 'http://api.reimaginebanking.com/customers/' + req.session.user.customerid + '/bills?key=2166680e61b8f43aaebf22492d3fbf65';
+        request.get(url, function(err, httpResp, body){
+        res.json({
+            success: 1,
+            payee: body
+        })
+    });
+});
+module.exports = router;
