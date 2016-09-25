51517937d77959bda8f0ba038fb25d0cc4a2189e
sahit <chsahit@gmail.com>
2016-09-25 04:10:22 -0400

    progress returns list of deposits

--- routes/progress.js
+++ routes/progress.js
@@ -9,16 +9,23 @@ var Goal = require('../models/goal');
 
 router.get('/progress', access.requireLogin, function(req, res) {
     if(req.session.user) {
-        request.get({url:'http://api.reimaginebanking.com/accounts/' + req.session.accountid + 'deposits?key=2166680e61b8f43aaebf22492d3fbf65' }, function optionalCallback(err, httpResponse, body)  {
+        request.get({url:`http://api.reimaginebanking.com/accounts/${req.session.user.accountid}/deposits?key=2166680e61b8f43aaebf22492d3fbf65` }, function optionalCallback(err, httpResponse, body)  {
             if (err) {
                 console.error('get failed: ', err);
                 res.json({
-                    'success': 1
+                    'success': 0
                 });
             } else {
-                console.log('success. ', httpResponse);
+                var response = "";
+                var i = 0;
+                console.log(JSON.parse(body)[0].amount);
+                for(var list in JSON.parse(body)) {
+                    response = response.concat(JSON.parse(body)[list].amount + ",");
+                }
+                console.log(response);
                 res.json({
-                    'success': 0
+                    'success': 1,
+                    data: response
                 });
             }
         });
